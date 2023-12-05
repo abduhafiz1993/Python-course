0a1f2f5a9b1d09efc27502d0647ef02da0952945
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 09:37:10 -0800

    upadate assert

--- Unit_test/test_square.py
+++ Unit_test/test_square.py
@@ -6,8 +6,26 @@ def main():
 
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared is not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared is not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared is not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared is not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared is not 0")
 
 if __name__ == "__main__":
     main()
