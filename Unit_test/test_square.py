2499646f1bc7f5ac17673e79d33f0083b3197e77
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 09:32:21 -0800

    Adding assert

--- Unit_test/test_square.py
+++ Unit_test/test_square.py
@@ -1,4 +1,4 @@
-from calculator import square
+from squares import square
 
 
 def main():
@@ -6,11 +6,8 @@ def main():
 
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4")
-    if square(3) != 9:
-        print("3 squared was not 9")
-
+    assert square(2) == 4:
+    assert square(3) == 9:
 
 if __name__ == "__main__":
     main()
