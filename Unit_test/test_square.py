0ac9eccc280387723c2275dcf4e40d7ecb3ed74f
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 09:34:44 -0800

    upadate assert

--- Unit_test/test_square.py
+++ Unit_test/test_square.py
@@ -6,8 +6,8 @@ def main():
 
 
 def test_square():
-    assert square(2) == 4:
-    assert square(3) == 9:
+    assert square(2) == 4
+    assert square(3) == 9
 
 if __name__ == "__main__":
     main()
