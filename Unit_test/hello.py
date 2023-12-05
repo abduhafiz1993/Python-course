8984b179cb2fc7f2c58e830bca97eca786ff984d
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 10:02:29 -0800

    upadate hello.py

--- Unit_test/hello.py
+++ Unit_test/hello.py
@@ -1,10 +1,10 @@
 def main():
     name = input("What's your name? ")
-    hello(name)
+    print(hello(name))
 
 
 def hello(to="world"):
-    print("hello,", to)
+    return f"hello, {to}"
 
 
 if __name__ == "__main__":
