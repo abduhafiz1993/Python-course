89364c44437aec4e688e75d93fa3b45765e14109
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 11:41:16 -0800

    Update file

---
+++ FileIO/students2.py
@@ -0,0 +1,10 @@
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
