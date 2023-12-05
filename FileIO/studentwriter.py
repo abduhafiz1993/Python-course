853abddc67a5b703667a0cbda4a4c9e20cd07e38
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-05 11:58:21 -0800

    Adding Student file

---
+++ FileIO/studentwriter.py
@@ -0,0 +1,8 @@
+import csv
+
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
