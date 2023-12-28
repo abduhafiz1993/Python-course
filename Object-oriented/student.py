504fe60317fc7bbc9e55d04645b6aec88b22aefa
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-27 22:02:30 -0800

    update student.py

--- Object-oriented/student.py
+++ Object-oriented/student.py
@@ -1,20 +1,32 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus= patronus
     
     
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🪄"
 
 
 def main():
     student = get_student()
-    print(student)
+    print(student.charm())
 
 
 def get_student():
