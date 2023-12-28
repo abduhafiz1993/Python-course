6fdc87a512f5714a6ec68b352477b5c1e30ecf6a
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-27 22:17:42 -0800

    adding lamba.py

---
+++ Functional_programming/lamba.py
@@ -0,0 +1,9 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
